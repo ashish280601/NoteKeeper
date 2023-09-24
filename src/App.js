ce920fe90970e62fa498a93025f7c144ffb7366d
ashish280601 <119057692+ashish280601@users.noreply.github.com>
2023-09-24 13:58:12 +0530

    installed react fontawesome

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import NoteKeeper from './components/Notekeeper';
+
+function App() {
+  return (
+    <>
+      <Navbar/>
+      <NoteKeeper/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Notekeeper.js
@@ -0,0 +1,27 @@
+const NoteKeeper = () => {
+    return(
+        <>
+            <form>
+                <input 
+                    placeholder="Take a note..."
+                />
+            </form>
+            <form>
+                <input 
+                    placeholder="Title"
+                />
+                <input
+                    placeholder="Tagline"
+                />
+                <input
+                    placeholder="Take a note..."
+                />
+                <button>
+                    
+                </button>
+            </form>
+        </>
+    )
+}
+
+export default NoteKeeper;
